c2cde6d2a830f38f1f2d089eada1b562b5ec5221
DGK <nassim.haddouche@gmail.com>
2020-12-01 15:34:36 +0100

    Created Entities

---
+++ src/Entity/Department.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DepartmentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DepartmentRepository::class)
+ */
+class Department
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $inhNum;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $reaBeds;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Region::class, inversedBy="departments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $region;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Town::class, mappedBy="department")
+     */
+    private $towns;
+
+    public function __construct()
+    {
+        $this->towns = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getInhNum(): ?string
+    {
+        return $this->inhNum;
+    }
+
+    public function setInhNum(string $inhNum): self
+    {
+        $this->inhNum = $inhNum;
+
+        return $this;
+    }
+
+    public function getReaBeds(): ?int
+    {
+        return $this->reaBeds;
+    }
+
+    public function setReaBeds(int $reaBeds): self
+    {
+        $this->reaBeds = $reaBeds;
+
+        return $this;
+    }
+
+    public function getRegion(): ?Region
+    {
+        return $this->region;
+    }
+
+    public function setRegion(?Region $region): self
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Town[]
+     */
+    public function getTowns(): Collection
+    {
+        return $this->towns;
+    }
+
+    public function addTown(Town $town): self
+    {
+        if (!$this->towns->contains($town)) {
+            $this->towns[] = $town;
+            $town->setDepartment($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTown(Town $town): self
+    {
+        if ($this->towns->removeElement($town)) {
+            // set the owning side to null (unless already changed)
+            if ($town->getDepartment() === $this) {
+                $town->setDepartment(null);
+            }
+        }
+
+        return $this;
+    }
+}
